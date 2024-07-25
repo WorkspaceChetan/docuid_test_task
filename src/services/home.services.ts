67fc8d007e26f4b05fead6de694b1326ec31db6b
Chetan Bardoliya <workspace.chetan@gmail.com>
2024-07-25 21:53:59 +0530

    remove env variable

--- src/services/home.services.ts
+++ src/services/home.services.ts
@@ -10,9 +10,7 @@ import {
 export class HomeServices {
   static getProcedues = async () => {
     try {
-      const res = await axios.get<Procedures[]>(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/procedure`
-      );
+      const res = await axios.get<Procedures[]>(`/api/procedure`);
 
       return res.data;
     } catch (err: any) {
