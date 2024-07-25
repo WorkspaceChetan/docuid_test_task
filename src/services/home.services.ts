feae1c5af126d96d8532378b35f7a92a8049013a
Chetan Bardoliya <workspace.chetan@gmail.com>
2024-07-25 21:56:20 +0530

    aded env

--- src/services/home.services.ts
+++ src/services/home.services.ts
@@ -10,7 +10,9 @@ import {
 export class HomeServices {
   static getProcedues = async () => {
     try {
-      const res = await axios.get<Procedures[]>(`/api/procedure`);
+      const res = await axios.get<Procedures[]>(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/procedure`
+      );
 
       return res.data;
     } catch (err: any) {
